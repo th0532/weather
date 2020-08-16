0984d8c30f9b38de0956046420b285bae0088abe
th0532 <kkllpp123456@naver.com>
2020-08-17 03:44:48 +0900

    Creating Main Layout

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Alert} from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  const [isLoading, setIsLoading] = useState(true);
+  
+   getLocation = async() =>{
+     try {
+        await Location.requestPermissionsAsync();
+        const {
+          coords:{latitude, longitude}
+        } = await Location.getCurrentPositionAsync()
+     } catch (error) {
+        Alert.alert("can't find you", "So sad");
+     }
+    
+  } 
+
+  useEffect(() => {
+    getLocation();
+  });
+
+    return (
+      <>
+      {isLoading && <Loading />}
+      </>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
